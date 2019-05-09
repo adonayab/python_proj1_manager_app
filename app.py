4c23d702b276f450ec6151c3348685b6a66c4576
adonayab <adonayab15@hotmail.com>
2019-05-08 22:21:24 -0500

    Seperated the tasks from the notes

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ app.secret_key = 'uahfkahdlfhuhkn8392uh'
 
 from users.routes import users
 from notes.note_routes import notes
+from notes.single_note_routes import singles
 from notes.task_routes import tasks
 
 app.register_blueprint(users)
 app.register_blueprint(notes)
+app.register_blueprint(singles)
 app.register_blueprint(tasks)
