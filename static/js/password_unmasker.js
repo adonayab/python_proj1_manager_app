5e80f7a6906791b93da777fcf792c8d3a9c6246a
adonayab <adonayab15@hotmail.com>
2019-06-20 11:49:00 -0500

    Fixed the bug that caused show password at login page not to work

--- static/js/password_unmasker.js
+++ static/js/password_unmasker.js
@@ -1,11 +1,20 @@
-function showHidePassFunc() {
-  let password = document.querySelector(".passwordElement");
-  let password2 = document.querySelector(".passwordElement2");
-  if (password.type === "password" && password2.type === 'password') {
-    password.type = "text";
-    password2.type = "text";
-  } else {
-    password.type = "password";
-    password2.type = "password";
+function showHidePassFunc(page) {
+  if (page === 'login') {
+    let password = document.querySelector(".passwordElement");
+    if (password.type === "password") {
+      password.type = "text";
+    } else {
+      password.type = "password";
+    }
+  } else if (page === 'register') {
+    let password = document.querySelector(".passwordElement");
+    let password2 = document.querySelector(".passwordElement2");
+    if (password.type === "password" && password2.type === 'password') {
+      password.type = "text";
+      password2.type = "text";
+    } else {
+      password.type = "password";
+      password2.type = "password";
+    }
   }
 }
