cd55ed568552a5c4e7225102d8035fb07d93ddba
adonayab <adonayab15@hotmail.com>
2020-04-10 21:03:28 -0500

    switched to sqlite

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bzmanager:bzmanager@localhost:3306/bzmanager'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bzmanager:bzmanager@localhost:3306/bzmanager'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_ECHO = False  # Will not echo querying in terminal
